e7fe6920e632ba8226fe65492514a880f711ceec
luisfelipemisiontic <luisfeliperomerolozano@gmail.com>
2022-12-05 21:30:28 -0500

    frontend create

---
+++ TiendaDelivery.App.Frontend/Pages/Productos/Create.cshtml.cs
@@ -0,0 +1,31 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using TiendaDelivery.App.Persistencia;
+using TiendaDelivery.App.Dominio;
+
+namespace TiendaDelivery.App.Frontend.pages.Productos
+{
+    public class CreateModel : PageModel
+    {
+        private readonly IRepositorioProducto _repoProducto;
+        public producto producto { get; set; }
+
+        public CreateModel(IRepositorioProducto repoProducto)
+        {
+            _repoProducto = repoProducto;
+        }
+
+        public void OnGet()
+        {
+            producto = new producto();
+        }
+
+        public IActionResult OnPost(producto producto)
+        {
+            _repoProducto.AddProducto(producto);
+            return RedirectToPage("Index");
+        }
+
+
+    }
+}
